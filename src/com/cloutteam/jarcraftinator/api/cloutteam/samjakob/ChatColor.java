f87b30e70da42470ffd66155c4788d40d839be7c
Sam Jakob Mearns <masterdoctor@clout-team.com>
2017-07-31 17:34:58 +0100

    Commit for rex's api

---
+++ src/com/cloutteam/jarcraftinator/api/cloutteam/samjakob/ChatColor.java
@@ -0,0 +1,4 @@
+package com.cloutteam.jarcraftinator.api.cloutteam.samjakob;
+
+public class ChatColor {
+}
--- src/com/cloutteam/jarcraftinator/packet/PacketType.java
+++
@@ -1,8 +0,0 @@
-package com.cloutteam.jarcraftinator.packet;
-
-public class PacketType {
-
-    public static final byte HANDSHAKE_PACKET = 0x00;
-    public static final byte REQUEST_PACKET = 0x00;
-
-}
