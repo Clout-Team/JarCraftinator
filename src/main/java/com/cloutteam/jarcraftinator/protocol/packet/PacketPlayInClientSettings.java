dd1601599524cb056358efe7da0407fcc3553ce6
Diogo Correia <diogo.correia2002@gmail.com>
2017-08-01 19:34:38 +0100

    Still working on the player login.

---
+++ src/main/java/com/cloutteam/jarcraftinator/protocol/packet/PacketPlayInClientSettings.java
@@ -0,0 +1,49 @@
+package com.cloutteam.jarcraftinator.protocol.packet;
+
+import com.cloutteam.jarcraftinator.utils.VarData;
+
+import java.io.DataInputStream;
+
+public class PacketPlayInClientSettings extends PacketIn {
+
+    private String locale;
+    private int viewDistance;
+    private int chatMode;
+    private boolean chatColors;
+    private int displayedSkinParts;
+    private int mainHand;
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public int getViewDistance() {
+        return viewDistance;
+    }
+
+    public int getChatMode() {
+        return chatMode;
+    }
+
+    public boolean isChatColors() {
+        return chatColors;
+    }
+
+    public int getDisplayedSkinParts() {
+        return displayedSkinParts;
+    }
+
+    public int getMainHand() {
+        return mainHand;
+    }
+
+    @Override
+    public void onReceive(int length, DataInputStream in) throws Exception {
+        locale = VarData.readVarString(in, VarData.readVarInt(in));
+        viewDistance = in.readByte();
+        chatMode = VarData.readVarInt(in);
+        chatColors = in.readBoolean();
+        displayedSkinParts = in.readUnsignedByte();
+        mainHand = VarData.readVarInt(in);
+    }
+}
