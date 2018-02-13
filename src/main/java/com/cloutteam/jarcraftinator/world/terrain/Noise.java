150fcf606e56c7abbb155d32fa383a490bfd9801
koboldthegreat <Simon.Bil@UGent.be>
2018-02-13 19:17:34 +0100

    little tests and not very neat

---
+++ src/main/java/com/cloutteam/jarcraftinator/world/terrain/Noise.java
@@ -0,0 +1,29 @@
+package com.cloutteam.jarcraftinator.world.terrain.noise;
+
+public abstract class Noise {
+	public final int octaves;
+	public final double scale;
+	public final double persistence;
+
+	public Noise (int octaves, double scale, double persistence){
+		this.octaves = octaves;
+		this.scale = scale;
+		this.persistence = persistence;
+	}
+
+	public double octavedNoise(int x, int z, int y) {
+		double noiseSum = 0;
+		double weight = 1;
+		double max = 0;
+		double freq = 1;
+		for (int oct = 0; oct < octaves; oct++) {
+			noiseSum += noise((double) x*scale*freq, (double) z*scale*freq, (double) y*scale*freq) * weight;
+			max += weight;
+			weight *= persistence;
+			freq *= 2;
+		}
+		return noiseSum/max; 
+	}
+
+	public abstract double noise(double x, double y, double z);
+}
